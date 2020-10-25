8f94b40784f9c40907d66ccdae473714b8cea5c1
deleeterious <deleeterious@gmail.com>
2020-10-25 17:02:13 +0200

    add image crop

--- src/components/Inputs/AvatarInput/AvatarInput.jsx
+++ src/components/Inputs/AvatarInput/AvatarInput.jsx
@@ -1,19 +1,33 @@
-import React, { memo } from 'react'
+import React, { memo, useState } from 'react'
+// react-image-crop
+import Cropper from 'react-easy-crop'
 // prop-types
 import T from 'prop-types'
 // react-hook-form
 import { useFormContext } from 'react-hook-form'
 // assets
 import { ReactComponent as AddIcon } from 'assets/icons/add.svg'
+// utils
+import getCroppedImg from 'utils'
 // components
 import AvatarImage from 'components/AvatarImage'
 import ValidationError from 'components/ValidationError'
-
+import Button from 'components/Button'
 // css
 import classes from './AvatarInput.module.css'
 
 const AvatarInput = ({ refRegister, errorMessage }) => {
   const { setValue, setError, watch } = useFormContext()
+
+  const [isCrop, setIsCrop] = useState(false)
+  const [image, setImage] = useState('')
+  const [crop, setCrop] = useState({ x: 0, y: 0 })
+  const [zoom, setZoom] = useState(1)
+  const [croppedAreaPixelsState, setCroppedAreaPixelsState] = useState(null)
+
+  const onCropComplete = (_, croppedAreaPixels) => {
+    setCroppedAreaPixelsState(croppedAreaPixels)
+  }
 
   const handleLoadLocalFile = (e) => {
     e.preventDefault()
@@ -28,10 +42,21 @@ const AvatarInput = ({ refRegister, errorMessage }) => {
           message: 'Image max size 1 MB'
         })
       } else {
-        setValue('avatarData', reader.result, { shouldDirty: true })
+        setImage(reader.result)
+        setIsCrop(true)
       }
     }
-    reader.readAsDataURL(file)
+
+    if (file) reader.readAsDataURL(file)
+  }
+
+  const handleApplyCrop = async (e) => {
+    e.preventDefault()
+
+    const croppedImage = await getCroppedImg(image, croppedAreaPixelsState)
+
+    setValue('avatarData', croppedImage, { shouldDirty: true })
+    setIsCrop(false)
   }
 
   return (
@@ -44,17 +69,42 @@ const AvatarInput = ({ refRegister, errorMessage }) => {
           ref={refRegister}
           className={classes.avatarData}
         />
-        <input
-          type="file"
-          name="avatar"
-          accept="image/png,image/jpeg,image/jpg"
-          id="avatar"
-          className={classes.fileInput}
-          onChange={handleLoadLocalFile}
-        />
+        {isCrop && (
+          <>
+            <Cropper
+              image={image}
+              crop={crop}
+              zoom={zoom}
+              aspect={1 / 1}
+              onCropChange={setCrop}
+              onCropComplete={onCropComplete}
+              onZoomChange={setZoom}
+              classes={{ containerClassName: classes.cropperCont }}
+            />
 
-        <AddIcon className={classes.addIcon} />
-        <div>add avatar</div>
+            <Button
+              className={classes.applyCropBtn}
+              handleClick={handleApplyCrop}
+            >
+              Apply
+            </Button>
+          </>
+        )}
+        {!isCrop && (
+          <>
+            <input
+              type="file"
+              name="avatar"
+              accept="image/png,image/jpeg,image/jpg"
+              id="avatar"
+              className={classes.fileInput}
+              onChange={handleLoadLocalFile}
+            />
+
+            <AddIcon className={classes.addIcon} />
+            <div>add avatar</div>
+          </>
+        )}
       </label>
       <ValidationError errorMessage={errorMessage} />
     </div>
