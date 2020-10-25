791fbfb2f7bebf678cc36de0d64fbcc5b7b031d6
deleeterious <deleeterious@gmail.com>
2020-10-25 11:45:24 +0200

    add pagination, move generate users to separate component

---
+++ src/components/GenerateUsersButton/GenerateUsersButton.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+// react-redux
+import { useDispatch } from 'react-redux';
+import { generateUsers } from 'redux/actions/users';
+// utils
+import { FakeUser } from 'utils';
+// components
+import Button from 'components/Button';
+// css
+import classes from './GenerateUsersButton.module.css';
+
+const GenerateUsersButton = () => {
+  const dispatch = useDispatch();
+
+  const handleGenerateUsers = (e) => {
+    e.preventDefault();
+    const newUsers = Array.from({ length: 50 }, () => ({ ...new FakeUser() }));
+    dispatch(generateUsers(newUsers));
+  };
+
+  return (
+    <Button handleClick={handleGenerateUsers} className={classes.generateBtn}>
+      Generate users
+    </Button>
+  );
+};
+
+export default GenerateUsersButton;
