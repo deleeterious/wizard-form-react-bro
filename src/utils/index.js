3a4ea141c52e5d33f544b74e96db7c729e1e68bb
deleeterious <deleeterious@gmail.com>
2020-10-25 17:02:13 +0200

    add image crop

--- src/utils/index.js
+++ src/utils/index.js
@@ -71,3 +71,34 @@ export class FakeUser {
     this.userName = faker.random.word();
   }
 }
+
+const createImage = (url) =>
+  new Promise((resolve, reject) => {
+    const image = new Image()
+    image.addEventListener('load', () => resolve(image))
+    image.addEventListener('error', (error) => reject(error))
+    image.src = url
+  })
+
+export default async function getCroppedImg(imageSrc, pixelCrop) {
+  const image = await createImage(imageSrc)
+  const canvas = document.createElement('canvas')
+  canvas.width = pixelCrop.width
+  canvas.height = pixelCrop.height
+  const ctx = canvas.getContext('2d')
+
+  ctx.fillStyle = '#ffffff'
+  ctx.fillRect(0, 0, canvas.width, canvas.height)
+  ctx.drawImage(
+    image,
+    pixelCrop.x,
+    pixelCrop.y,
+    pixelCrop.width,
+    pixelCrop.height,
+    0,
+    0,
+    pixelCrop.width,
+    pixelCrop.height
+  )
+  return canvas.toDataURL('image/jpeg')
+}
